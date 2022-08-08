7eac03565ebefa756b26ee2b736dff93eafb7d00
Tom Yandell <tom.yandell@axetrading.com>
2022-08-08 14:48:43 +0100

    Add privileged mode
    No docker without this

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_codebuild_project" "default" {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = false
+    privileged_mode             = true
     type                        = "LINUX_CONTAINER"
   }
 
